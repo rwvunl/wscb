d0223941ce9b007c2ed6435c46822dc22c24e2bc
rwvunl <r.wang3@student.vu.nl>
2024-02-13 14:10:05 +0100

    final version

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify
 import random
 import string
 import json
-import re
-app = Flask(__name__)  # 用于存储URL映射关系的字典
-url_id_mapping = dict()
+import threading
+from utils import base62_encode, is_valid_url
+
+app = Flask(__name__)
+
 id_url_mapping = dict()
+
+global increment
+increment = 0
+
+lock = threading.Lock()
 
 def generate_id():
     """Generates ID."""
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(6))  # Generating a 6-character long ID
+    # Generate random part
+    random_part = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    # Generate incremental part
+    if random_part in id_url_mapping.keys():
+        increment += 1
+        return random_part + base62_encode(increment)
+    return random_part
 
-def is_valid_url(url):
-    """Check if the provided URL is valid."""
-    regex = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return re.match(regex, url) is not None
 
 @app.route('/', methods=['POST'])
 def create_short_url():
@@ -29,34 +31,25 @@ def create_short_url():
     long_url = request.json.get('value')
     if not long_url:
         return jsonify({'error': 'Long URL is required in the request body'}), 400
-
     if not is_valid_url(long_url):
         return jsonify({'error': 'Invalid URL format'}), 400
-
-    id = generate_id()
-
-    url_id_mapping[long_url] = id
-    id_url_mapping[id] = long_url
-
-    short_url = 'http://127.0.0.1:5000/{id}'.format(id=id)
+    with lock:
+        id = generate_id()
+        id_url_mapping[id] = long_url
+    # short_url = 'http://127.0.0.1:5000/{id}'.format(id=id)
     return jsonify({'id': id}), 201
-
 
 @app.route('/', methods=['GET'])
 def list_urls():
-    """Returns a list of all keys (IDs) or all long URLs."""
-    all_keys = list(url_id_mapping.keys())
-    all_urls = list(url_id_mapping.values())
-    return jsonify({'keys': all_keys, 'urls': all_urls}), 200
-
+    return jsonify(id_url_mapping), 200
 
 @app.route('/<id>', methods=['GET'])
 def redirect_to_long_url(id):
     """Redirect the user to the long URL corresponding to the given ID."""
     try:
-        long_url = id_url_mapping[id]
+        with lock:
+            long_url = id_url_mapping[id]
         return jsonify({'value': long_url}), 301
-        # return redirect(long_url, code=301)
     except KeyError:
         return jsonify({'error': 'Short URL not found'}), 404
 
@@ -67,7 +60,6 @@ def update_long_url(id):
     print(request.data)
     data = json.loads(request.data.decode('utf-8'))
     new_url = data.get('url')
-    # new_url = request.json.get('url')
     if not new_url:
         return jsonify({'error': 'New URL is required in the request body'}), 400
     if id not in id_url_mapping.keys():
@@ -75,8 +67,8 @@ def update_long_url(id):
     if not is_valid_url(new_url):
         return jsonify({'error': 'Invalid URL format'}), 400
     # Update the URL behind the given ID
-    url_id_mapping[new_url] = id
-    id_url_mapping[id] = new_url
+    with lock:
+        id_url_mapping[id] = new_url
     return jsonify({'value': new_url}), 200
 
 
@@ -85,18 +77,17 @@ def delete_url(id):
     """Deletes the given short URL/ID."""
     if id not in id_url_mapping.keys():
         return jsonify({'error': 'Short URL not found'}), 404
-
     # Delete the short URL/ID
-    del url_id_mapping[id_url_mapping[id]]
-    del id_url_mapping[id]
+    with lock:
+        del id_url_mapping[id]
     return '', 204
 
 
 @app.route('/', methods=['DELETE'])
 def delete_all_urls():
     """Deletes all ID/URL pairs"""
-    url_id_mapping.clear()
-    id_url_mapping.clear()
+    with lock:
+        id_url_mapping.clear()
     return jsonify({'message': 'All ID/URL pairs have been deleted'}), 404
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
+import re
 ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 def base62_encode(num, alphabet=ALPHABET):
     """
     10进制转62进制
@@ -31,6 +31,17 @@ def base62_decode(string, alphabet=ALPHABET):
 
     return num
 
+def is_valid_url(url):
+    """Check if the provided URL is valid."""
+    regex = re.compile(
+        r'^(?:http|http)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url) is not None
+
 if __name__ == '__main__':
-    print(base62_encode(4302842))
+    print(base62_encode(999999999999999999999999999999))
     print(base62_decode('i3mG'))
